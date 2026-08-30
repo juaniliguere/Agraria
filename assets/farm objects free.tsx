a746f00c2df2c355093d17dd548065abf7de4372
juaniliguere <juaniliguere13@gmail.com>
2026-08-30 20:15:51 -0300

    Pruebas colisiones con mapa y personaje

---
+++ assets/farm objects free.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="farm objects free" tilewidth="16" tileheight="16" tilecount="99" columns="9">
+ <image source="farm objects free.png" width="144" height="191"/>
+</tileset>
---
+++ assets/farm.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="farm" tilewidth="4" tileheight="4" tilecount="1692" columns="36">
+ <image source="../Tiny Wonder Farm Free/farm objects free.png" width="144" height="191"/>
+</tileset>
