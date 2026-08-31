59bd8bca9b293cbef5ed431f4abcca996b388dd5
Brunella Daiara Urriza Morán <urrizamoran.brunella.et352026@gmail.com>
2026-08-31 18:29:57 -0300

    Actualizacion automatica

--- subir.bat
+++ subir.bat
@@ -1,10 +1,15 @@
 @echo off
-echo Guardando y subiendo cambios a GitHub...
+echo 1. Bajando cambios de GitHub...
+git pull --rebase origin main
+
+echo.
+echo 2. Guardando y subiendo tus cambios...
 git add .
 git commit -m "Actualizacion automatica"
-git push
+git push origin main
+
 echo.
 echo ---------------------------------------
-echo Proceso finalizado.
+echo ¡Proceso finalizado con exito!
 echo ---------------------------------------
 pause
